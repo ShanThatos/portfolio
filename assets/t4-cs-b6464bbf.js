23a3935d074b4106212aad107e8a34bff45ae773
ShanThatos <shanthkoka@gmail.com>
2024-06-28 02:10:21 -0500

    Updates

--- assets/t4-cs-b6464bbf.js
+++
@@ -1,1 +0,0 @@
-import e from"./csharp-c15b8f97.js";import p from"./t4-templating-ef79f002.js";import"./Markdown-15049ecd.js";import"./index-2b67b052.js";import"./date-display-7f7ac77c.js";a.displayName="t4-cs";a.aliases=["t4"];function a(t){t.register(e),t.register(p),t.languages.t4=t.languages["t4-cs"]=t.languages["t4-templating"].createT4("csharp")}export{a as default};
